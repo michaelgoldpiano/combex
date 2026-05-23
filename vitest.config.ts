435b91b165291827ad5f24352ae766377ce9c7e5
Michael Gold <michaelgoldpiano@gmail.com>
2026-05-23 16:37:40 -0400

    Added typechecking to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    typecheck: {
+      enabled: true,
+      checker: 'tsc',
+      include: ['**/*.test.ts'],
+    },
+  },
+});
